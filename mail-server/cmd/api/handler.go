b512cb093f3de45b491bd00b2eadf5aa10184399
Shuhai Liao <shuai190060roy@gmail.com>
2023-10-17 14:21:36 +0200

    mail-server

---
+++ mail-server/cmd/api/handler.go
@@ -0,0 +1,35 @@
+package main
+
+import "net/http"
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessge struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+	var requestPayload mailMessge
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+	payload := JsonResponse{
+		Error:   false,
+		Message: "sent to " + requestPayload.To,
+	}
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
